b22847b896f4aff96fccf00c88ecbba4d806a05f
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-05-23 22:37:52 +0700

    Add black bishop and king

--- src/BlackB.c
+++ src/BlackB.c
@@ -4,6 +4,23 @@
 #include <string.h>
 char BlackB(char Desk[][9], int n, char Str[10])
 {
+    int i, t, t1, d1, d2;
+    for (i = 1; i < n; i++)
+        if (Str[0] == Desk[8][i])
+            t = i;
 
- return 0;
+    for (i = 1; i < n; i++)
+        if (Str[3] == Desk[8][i])
+            t1 = i;
+
+    d1 = 8 - (Str[1] - 48);
+    d2 = 8 - (Str[4] - 48);
+    if (d2 > 9 || d2 < 0) {
+        printf("Incncorrect turn\n");
+    } else {
+        Desk[d1][t] = '\0';
+        Desk[d2][t1] = 'b';
+    }
+
+    return 0;
 }
